63c254089e5b9b19069770af848712d28df4e855
AnarresNative <louisdebnam@proton.me>
2024-12-17 20:45:01 +0000

    First attempt at setting up a get entry function

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from . import views, util
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.get_entry, name="wiki")
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+import os
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def get_entry(request, title):
+    return render(request, "encyclopedia/layout.html", {
+        "title": title,
+        "markdown": util.get_entry(title)
     })
 
